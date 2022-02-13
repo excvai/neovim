a824f0c489d5cc32b7cd7f0894569a980083d85d
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2022-02-13 15:31:45 +0200

    add notify messages

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -23,11 +23,13 @@ vim.g.nvim_tree_icons = {
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
+  vim.notify("nvim-tree require failed")
   return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
+  vim.notify("nvim-tree.config require failed")
   return
 end
 

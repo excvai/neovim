ae58ee9d55cc75faea323e19a3a9269973c4b63b
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2022-02-14 11:16:54 +0200

    null-ls: add eslint code_actions

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,11 +8,13 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
-    diagnostics.eslint
+    diagnostics.eslint,
+    code_actions.eslint,
+		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } })
 	},
 })

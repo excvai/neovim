a353069a31e840c6b42ffc2944ded10c1eea8e18
excvai <vladyslav.katrych.main@gmail.com>
2022-02-28 00:17:36 +0200

    Add autocmd to restore previous cursor position

--- lua/user/helper-functions.lua
+++ lua/user/helper-functions.lua
@@ -16,3 +16,11 @@ function! s:DeleteAllBuffers()
 endfunction
 com! Ba call s:DeleteAllBuffers()
 ]]
+
+-- Restore previous cursor position
+vim.cmd [[
+autocmd BufReadPost *
+  \ if line("'\"") >= 1 && line("'\"") <= line("$") |
+  \   exe "normal! g`\"" |
+  \ endif
+]]

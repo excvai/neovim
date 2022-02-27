e3548434a9f034c6f446ee3422628737448e3fe7
excvai <vladyslav.katrych.main@gmail.com>
2022-02-27 22:42:00 +0200

    remove lsp_signature

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -7,4 +7,3 @@ end
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
-require "user.lsp.lsp-signature"
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,7 +90,6 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  use "ray-x/lsp_signature.nvim" -- show function signature when you type
   use "jose-elias-alvarez/nvim-lsp-ts-utils"
 
   -- Telescope

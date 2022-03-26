54c0d6aa68ca15d137de3b2e7a2b1170bfe14378
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2022-03-26 13:06:04 +0200

    change quick-scope options

--- lua/user/quick-scope.lua
+++ lua/user/quick-scope.lua
@@ -1,5 +1,6 @@
+vim.api.nvim_set_var('qs_highlight_on_keys', {'f', 'F'})
+
 vim.cmd [[
-let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
 highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
 highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
 ]]

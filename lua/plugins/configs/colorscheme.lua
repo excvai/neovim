fba143d747baa1d108f6c6b178bd6b9f4ffd53c5
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2022-06-05 16:29:08 +0300

    Enable italic comment

--- lua/plugins/configs/colorscheme.lua
+++ lua/plugins/configs/colorscheme.lua
@@ -1,5 +1,6 @@
 local colorscheme = "vscode"
 vim.g.vscode_style = "dark"
+vim.g.vscode_italic_comment = 1
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
